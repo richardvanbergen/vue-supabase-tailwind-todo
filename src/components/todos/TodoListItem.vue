98575d9816bd05a8b8ed452923dd22448da20218
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-01-25 00:07:50 +0900

    Formatting for title in todo list items

--- src/components/todos/TodoListItem.vue
+++ src/components/todos/TodoListItem.vue
@@ -1,6 +1,9 @@
 <template>
-  <li>
-    <slot name="title" />
+  <li class="p-2">
+    <strong class="block" role="heading" aria-level="3">
+      <slot name="title" />
+    </strong>
+
     <slot name="description" required />
   </li>
 </template>
