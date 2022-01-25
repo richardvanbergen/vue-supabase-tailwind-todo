67f87647ae33260279169454cca64e9e6ee5da5d
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-01-25 16:10:13 +0900

    Implement edit form component on page

--- src/views/TodoEdit.vue
+++ src/views/TodoEdit.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
 import Hero from '@/components/Hero.vue'
 import FloatingContentBox from '@/components/FloatingContentBox.vue'
-import TodoListItem from '@/components/todos/TodoListItem.vue'
-import TodoList from '@/components/todos/TodoList.vue'
-import { useTodosStore } from '@/stores/todo'
 import ErrorNotification from '@/components/ErrorNotification.vue'
-import ButtonPrimary from '@/components/ButtonPrimary.vue'
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import TodoItemEditForm from '@/components/todos/TodoItemEditForm.vue'
 </script>
 
 <script lang="ts">
@@ -15,66 +11,26 @@ export default {
     return {
       loading: false,
       error: '',
-      items: [] as { title: string; description: string }[],
     }
   },
-  async created() {
-    await this.fetchList()
-  },
-  methods: {
-    async fetchList() {
-      this.loading = true
-      const todosStore = useTodosStore()
-      try {
-        const response = await todosStore.fetchActiveTodos()
-        if (response.status === 200 || response.status === 201) {
-          this.items = todosStore.todos
-        } else {
-          this.error = `${response.status}: ${response.statusText}`
-        }
-      } catch (error) {
-        if (typeof error === 'string') {
-          this.error = error
-        }
-      }
-      // this.loading = false
-    },
-  },
+  methods: {},
 }
 </script>
 
 <template>
   <article class="flex flex-col w-screen max-w-4xl gap-8 m-auto">
     <div class="flex items-center justify-between">
-      <Hero heading="🚀 Todos" />
-
-      <ButtonPrimary>Add New</ButtonPrimary>
+      <Hero heading="🚀 Create New" />
     </div>
 
     <FloatingContentBox>
-      <ErrorNotification>
+      <ErrorNotification v-if="error">
         {{ error }}
-        <button
-          class="px-4 py-1 text-white bg-red-700 rounded-sm"
-          v-on:click.prevent="fetchList"
-        >
-          Retry
-        </button>
       </ErrorNotification>
 
-      <PulseLoader />
+      <PulseLoader v-if="loading" />
 
-      <TodoList>
-        <TodoListItem :key="index" v-for="(item, index) in items">
-          <template #title>
-            {{ item.title }}
-          </template>
-
-          <template #description>
-            {{ item.description }}
-          </template>
-        </TodoListItem>
-      </TodoList>
+      <TodoItemEditForm />
     </FloatingContentBox>
   </article>
 </template>
