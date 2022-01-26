3c91f17f61ed10d45c2bab8b2d3580f827d9fa00
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-01-26 20:41:14 +0900

    Removing old error notification component.

--- src/components/ErrorNotification.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="bg-red-600 text-white text-lg py-2 px-4 font-semibold rounded-md shadow-xl flex justify-between items-center gap-4 flex-nowrap"
-  >
-    <slot />
-  </div>
-</template>
