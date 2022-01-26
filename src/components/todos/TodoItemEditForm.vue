7167ac2074f3ec83248824798a7748eab7d53662
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-01-26 21:15:03 +0900

    Convert edit form to the composition API

--- src/components/todos/TodoItemEditForm.vue
+++ src/components/todos/TodoItemEditForm.vue
@@ -1,50 +1,46 @@
-<script lang="ts">
+<script setup lang="ts">
 import todoSchema from '@/data/validation/todo-schema'
 import {
   validateSchema,
   ValidationPathErrorMap,
 } from '@/data/validation/validate'
 import { Todo } from '@/stores/todo'
-import { defineComponent, PropType } from 'vue'
+import { reactive, ref, toRefs } from 'vue'
 
-export default defineComponent({
-  props: {
-    submitHandler: {
-      type: Function as PropType<(data: Todo) => Promise<void>>,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      errors: {} as ValidationPathErrorMap,
-    }
-  },
-  methods: {
-    async checkForm(e: Event) {
-      e.preventDefault()
-      const target = e.target as HTMLFormElement
-      const formData = new FormData(target)
-      const todo = {
-        title: formData.get('title')?.toString() ?? '',
-        description: formData.get('description')?.toString() ?? '',
-      }
+const props = defineProps<{
+  submitHandler: (data: Todo) => Promise<void>
+}>()
 
-      const validationResult = await validateSchema(todoSchema, todo)
-      if (validationResult === true) {
-        this.submitHandler(todo)
-      } else {
-        this.errors = validationResult
-      }
-    },
-  },
+const { submitHandler } = toRefs(props)
+const todo = reactive({
+  title: '',
+  description: '',
 })
+
+const errors = ref<ValidationPathErrorMap>({})
+
+const checkForm = async (e: Event) => {
+  e.preventDefault()
+  const validationResult = await validateSchema(todoSchema, todo)
+  if (validationResult === true) {
+    submitHandler.value(todo)
+  } else {
+    errors.value = validationResult
+  }
+}
 </script>
 
 <template>
   <form v-on:submit="checkForm" class="flex flex-col space-y-4">
     <div>
       <label for="title">Task Title</label>
-      <input type="text" name="title" id="title" class="w-full mt-2" />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        v-model="todo.title"
+        class="w-full mt-2"
+      />
       <span v-if="errors.title" class="text-red-500">{{
         errors.title.message
       }}</span>
@@ -55,6 +51,7 @@ export default defineComponent({
         name="description"
         id="description"
         class="w-full mt-2"
+        v-model="todo.description"
         cols="30"
         rows="10"
       ></textarea>
